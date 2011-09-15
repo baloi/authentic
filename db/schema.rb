2967255860639e3bddf640e5d60439b28897f9e3
baloi <baloi.galia@gmail.com>
2011-09-14 22:39:34 -0400

    users in sqlite db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110725124727) do
+ActiveRecord::Schema.define(:version => 20110915022314) do
+
+  create_table "residents", :force => true do |t|
+    t.string   "name"
+    t.integer  "pt_minutes_per_day"
+    t.integer  "ot_minutes_per_day"
+    t.integer  "pt_days_per_week"
+    t.integer  "ot_days_per_week"
+    t.boolean  "active"
+    t.string   "room"
+    t.string   "insurance"
+    t.datetime "eval_date"
+    t.integer  "physical_therapist_id"
+    t.integer  "occupational_therapist_id"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
