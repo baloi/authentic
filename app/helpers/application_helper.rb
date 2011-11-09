9b95540a93ee4bdee19b49d22fb1934c964fe48d
baloi <baloi.galia@gmail.com>
2011-11-08 22:54:09 -0500

    therapists and resident entry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,4 +31,22 @@ module ApplicationHelper
     a_tag
   end
 
+  def physical_therapist_select 
+    a_tag = "<select id='resident_physical_therapist_id' name='resident[physical_therapist_id]'>"
+
+    @therapists.each do |therapist|
+      selected = ''
+      if @resident.id != nil
+        if @resident.physical_therapist.id != nil && therapist == @resident.physical_therapist.id
+          selected = " selected='yes'"
+        end
+      end
+      a_tag += "<option value='#{therapist.id}'#{selected}>#{therapist.name}</option>"
+    end
+
+    a_tag += "</select>"
+    a_tag
+  end
+
+
 end
